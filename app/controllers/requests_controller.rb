0f494c7365aa841aae4f72583e0106793e428bd6
Sybil <sybil.deboin@gmail.com>
2016-01-24 21:32:31 +0100

    Retrieve results from Amazon API with Vacuum
    Took a break before and this break will become a real pause after this
    commit

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -13,6 +13,19 @@ end
 
 def show
   @request = Request.find(params[:id])
+  amazon = Vacuum.new
+amazon.configure(
+    aws_access_key_id: Rails.application.secrets.amazon_access_key,
+    aws_secret_access_key: Rails.application.secrets.amazon_secret_key,
+    associate_tag: 'Project'
+)
+  @response = amazon.item_search(
+    query: {
+      'Keywords' => @request.keywords,
+      'SearchIndex' => 'All'
+    }
+  )
+
 end
 
 def index
